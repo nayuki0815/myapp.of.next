23a8477da0b2f31991b9593659c159546a90d5b9
nayuki0815 <nakano.yuuki.0815@gmail.com>
2021-04-13 13:05:12 +0900

    テスト

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          welcome to　なかWeb サイト！<a href="https://nextjs.org">Next.js!</a>
+        　Web サイト！<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
